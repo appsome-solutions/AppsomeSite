2affc4799aa75005980bd72a9ea4993055f7d7e3
JanikOskar <oskar.janik@gmail.com>
2020-01-19 14:52:57 +0100

    Service works

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import { Header } from './Header/Header';
 import { Banner } from './Banner/Banner';
-import { TheProcess } from './TheProcess/TheProcess';
 import { OurServices } from './OurService/OurService';
 export const Home: FunctionComponent = () => (
   <>
     <Header />
     <Banner />
-    <TheProcess />
     <OurServices />
   </>
 );
