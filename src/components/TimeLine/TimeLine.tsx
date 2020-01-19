d95ce6f09f97959ff4c051cfbad2cc20c9412121
JanikOskar <oskar.janik@gmail.com>
2020-01-19 15:06:31 +0100

    Newsleter done

---
+++ src/components/TimeLine/TimeLine.tsx
@@ -0,0 +1,67 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { Icon } from 'components/Icon/Icon';
+export type PositionText = 'right' | 'left';
+export interface ElementProps {
+  title?: string;
+  content?: string | JSX.Element;
+  contentPosition?: PositionText;
+  svgLink: string;
+  positionText: PositionText;
+}
+
+const PositionAbsolute = styled.div`
+  position: absolute;
+`;
+const ProcessTimeLineElement = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const PositionWholeText = styled(PositionAbsolute)<{
+  positionText?: PositionText;
+}>`
+  ${props => (props.positionText === 'left' ? 'right:188px;' : 'left:188px;')}
+`;
+const Title = styled.h5`
+  color: ${props => props.theme.colors.main.secondary};
+`;
+const IdeaContent = styled.pre`
+  ${props => props.theme.typography.body1};
+`;
+const TextPosition = styled.div<{
+  contentPosition?: PositionText;
+}>`
+  text-align: ${props => (props.contentPosition === 'left' ? 'left' : 'right')};
+`;
+const Circle = styled.div`
+  height: 160px;
+  width: 160px;
+  background-color: ${props => props.theme.colors.main.primary};
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const IdeaIconStyle = styled(Icon)`
+  width: 128px;
+  height: 128px;
+  background: ${props => props.theme.colors.utils.background.mid.color};
+`;
+export const ProcessTimeLine: FunctionComponent<ElementProps> = (props: ElementProps): JSX.Element => {
+  const { title, content, svgLink } = props;
+  return (
+    <ProcessTimeLineElement>
+      <Circle>
+        <IdeaIconStyle svgLink={svgLink} />
+      </Circle>
+      <PositionWholeText positionText={props.positionText}>
+        <TextPosition contentPosition={props.contentPosition}>
+          <Title>{title}</Title>
+          <IdeaContent>{content}</IdeaContent>
+        </TextPosition>
+      </PositionWholeText>
+    </ProcessTimeLineElement>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { Header } from './Header/Header';
 import { Banner } from './Banner/Banner';
-import { TheProcess } from './TheProcess/TheProcess';
 import { OurServices } from './OurService/OurService';
 import { PortfolioStyled } from './Portfolio/Portfolio';
 import { ContactStyled } from './Contact/Contact';
@@ -10,7 +9,6 @@ export const Home: FunctionComponent = () => (
   <>
     <Header />
     <Banner />
-    <TheProcess />
     <OurServices />
     <PortfolioStyled />
     <ContactStyled />
