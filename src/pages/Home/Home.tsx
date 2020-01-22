3b02f50b62bc49b3608a3e8b80070d7d8f395ea6
JanikOskar <oskar.janik@gmail.com>
2020-01-22 11:26:56 +0100

    Conflicts resolved

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import { Header } from './Header/Header';
 import { Banner } from './Banner/Banner';
+import { TheProcess } from './TheProcess/TheProcess';
+import { OurServices } from './OurService/OurService';
 import { ContactStyled } from './Contact/Contact';
 export const Home: FunctionComponent = () => (
   <>
     <Header />
     <Banner />
+    <TheProcess />
+    <OurServices />
     <ContactStyled />
   </>
 );
