ce8405d2d991417807e8fca9d0edb6469ae72cae
JanikOskar <oskar.janik@gmail.com>
2020-01-30 13:56:37 +0100

    PrivacyPolicy

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import { Header } from './Header/Header';
-import { Banner } from './Banner/Banner';
-import { OurServices } from './OurService/OurService';
-import { Newsletter } from './Newsleter/Newsleter';
-import { TheProcess } from './TheProcess/TheProcess';
-import { Footer } from './Footer/Footer';
-import { ContactStyled } from './Contact/Contact';
-import { PortfolioStyled } from './Portfolio/Portfolio';
+
+import { PrivacyPolicy } from './PrivatePolicy/PrivatePolicy';
 
 export const Home: FunctionComponent = () => (
   <>
-    <Header />
-    <Banner />
-    <TheProcess />
-    <OurServices />
-    <PortfolioStyled />
-    <ContactStyled />
-    <Newsletter />
-    <Footer />
+    <PrivacyPolicy />
   </>
 );
