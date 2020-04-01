bef7de918480e670f66e51c01b1235ff7504ba7d
Patryk Janik <pumanitro@gmail.com>
2020-04-01 18:03:12 +0200

    Fixed section Title display on mobile.

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -9,26 +9,32 @@ export interface SectionProps {
 }
 
 const Title = styled.h4`
-  margin-left: 8px;
+  margin-left: 16px;
   margin-top: 14px;
   color: ${props =>
     props.color === 'primary' ? props.theme.colors.utils.background.mid.color : props.theme.colors.utils.text.dark};
 `;
 
 const SectionContent = styled.div`
-  height: 30px;
   display: flex;
   align-items: center;
-  border-left: 8px solid
-  margin-bottom:8px;
-    color:${props =>
-      props.color === 'primary' ? props.theme.colors.main.tetiary : props.theme.colors.utils.text.dark};
+  margin-bottom: 8px;
+  position: relative;
+`;
+
+const SectionSeparator = styled.span`
+  height: 30px;
+  border-left: 8px solid;
+  position: absolute;
+  top: 26px;
+  color: ${props => (props.color === 'primary' ? props.theme.colors.main.tetiary : props.theme.colors.utils.text.dark)};
 `;
 
 export const SectionTitle: FunctionComponent<SectionProps> = (props: SectionProps) => {
   const { section } = props;
   return (
-    <SectionContent color={props.color}>
+    <SectionContent>
+      <SectionSeparator color={props.color} />
       <Title color={props.boxColor}>{section}</Title>
     </SectionContent>
   );
