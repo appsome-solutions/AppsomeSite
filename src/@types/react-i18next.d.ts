b1f6221346f8296f548dfa6ebbb2d2ac6ba9083e
Patryk Janik <pumanitro@gmail.com>
2020-04-02 18:23:38 +0200

    Attempt of react-snap performance page optimisation.

--- src/@types/react-i18next.d.ts
+++
@@ -1,7 +0,0 @@
-import Dictionary from 'global/Langs/pl';
-
-export type TranslationsType = typeof Dictionary.pl.translation;
-
-declare module 'react-i18next' {
-  export function t(translate: TranslationsType): string;
-}
--- src/@types/styled-components.d.ts
+++
@@ -1,8 +0,0 @@
-import CoreTheme from 'global/Themes/CoreTheme';
-
-export type ThemeInterface = typeof CoreTheme;
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends ThemeInterface {}
-}
