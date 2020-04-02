25d4386a5110f31e3580b5d6d6381024e72fe1bb
Patryk Janik <pumanitro@gmail.com>
2020-04-02 23:12:01 +0200

    Added OurServices to be prefetched instead of lazy loaded.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, Suspense } from 'react';
 import { Banner } from './Banner/Banner';
 import { TheProcess } from './TheProcess/TheProcess';
+import { OurServices } from './OurService/OurService';
 
 const LazyLoadedSections = React.lazy(() => import('./LazyLoadedSection/LazyLoadedSections'));
 
@@ -8,6 +9,7 @@ export const Home: FunctionComponent = () => (
   <>
     <Banner />
     <TheProcess />
+    <OurServices />
     <Suspense fallback={<div>Loading...</div>}>
       <LazyLoadedSections />
     </Suspense>
--- src/pages/Home/LazyLoadedSection/LazyLoadedSections.tsx
+++ src/pages/Home/LazyLoadedSection/LazyLoadedSections.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { PortfolioStyled } from '../Portfolio/Portfolio';
 import { ContactStyled } from '../Contact/Contact';
 import { Newsletter } from '../Newsleter/Newsleter';
-import { OurServices } from '../OurService/OurService';
 import { FirebaseProvider } from 'global/Firebase/Firebase';
 
 // eslint-disable-next-line react/display-name
 export default () => (
   <FirebaseProvider>
-    <OurServices />
     <PortfolioStyled />
     <ContactStyled />
     <Newsletter />
